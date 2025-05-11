575ba9aaaf0125bfeaaa80641e8d9d90dfd4ac19
Ajay-oraon <jaioraon17@gmail.com>
2025-05-11 14:07:46 +0530

    fix:home

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
+app.get("/", async (req, res) => {
+  res.send("This is the Home Page of the Website");
+});
 app.use("/auth", authRoutes);
 app.use("/api/user", spotifyRoutes);
 
